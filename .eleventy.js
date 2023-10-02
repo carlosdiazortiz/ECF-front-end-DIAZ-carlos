89e303648710d23eb6bc12e98cc2c53ff581e7c5
Carlos Diaz <carlos.diaz@outlook.fr>
2023-10-02 20:18:50 +0200

    fixed admin panel

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig){
   eleventyConfig.addPassthroughCopy("./src/style.css");
   eleventyConfig.addPassthroughCopy("./src/script.js");
   eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy("./src/admin");
   return {
     dir: {
       input: "src",
