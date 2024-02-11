67fe506ffb132c10d6eddd415443884f8148bf44
Carlos Diaz <carlos.diaz@outlook.fr>
2024-02-11 22:01:34 +0100

    new changes

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 document.querySelector(".xIcon").addEventListener("click", function() {
   document.querySelector("menu").classList.toggle('showMenu');
-  document.querySelector(".big-logo").classList.toggle('big-logo-change');
+  document.querySelector(".big-logo").classList.toggle('big-logo-push');
   this.classList.toggle("change");
 });
 
