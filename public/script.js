afb9c162d0ae82b5fdc0fb5d8cf25c3d457d147e
Carlos Diaz <carlos.diaz@outlook.fr>
2024-03-02 11:09:00 +0100

    new changes

--- public/script.js
+++ public/script.js
@@ -1,7 +1,28 @@
 document.querySelector(".xIcon").addEventListener("click", function() {
   document.querySelector("menu").classList.toggle('showMenu');
-  document.querySelector(".big-logo").classList.toggle('big-logo-push');
   this.classList.toggle("change");
+  document.querySelector(".big-logo").classList.toggle('big-logo-push');
 });
+
+var modal = document.getElementById("myModal");
+
+var img = document.getElementsByClassName("image");
+for(var i = 0; i < img.length; i++) {
+  img[i].addEventListener("click", function() {
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  })
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.addEventListener("click", function() {
+  modal.style.display ="none";
+})
+
+  
+
 
 
